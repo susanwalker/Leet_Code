6069eb649c073a32ce586386b0c06086a2f36aca
Susan Walker <susanwalker8@gmail.com>
2021-02-19 14:04:30 -0500

    Add new exercise

---
+++ edabit/applying_discounts.rb
@@ -0,0 +1,21 @@
+module ApplyingDiscounts
+  def self.discounts(arr, discount_num)
+    arr.map do |num|
+      num * (discount_num.to_i / 100.0)
+    end
+  end
+end
+
+require 'rspec'
+
+describe ApplyingDiscounts do
+  describe '.discounts' do
+    context 'when given an array and percentage of discount' do
+      let!(:array) { [2, 4, 6, 11] }
+
+      it 'returns an array with the discount removed from each num' do
+        expect(ApplyingDiscounts.discounts(array, "50%")).to eq([1, 2, 3, 5.5])
+      end
+    end
+  end
+end
