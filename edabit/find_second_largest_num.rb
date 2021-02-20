a1659017dbc45cfdd2d0cd07a241470f8a943c92
Susan Walker <susanwalker8@gmail.com>
2021-02-20 12:04:06 -0500

    Correction of find_second_largest_num

--- edabit/find_second_largest_num.rb
+++ edabit/find_second_largest_num.rb
@@ -1,7 +1,7 @@
 module FindSecondLargest
   def self.find_second_max(array)
-    array.max
-    array.pop
+    max = array.max
+    array.delete(max)
     array.max
   end
 end
@@ -11,7 +11,7 @@ require 'rspec'
 describe FindSecondLargest do
   describe '.find_second_max' do
     context 'when given an array of nums' do
-      let!(:array) { [10, 40, 30, 20, 50] }
+      let!(:array) { [10, 40, 30, 50, 20] }
 
       it 'returns the second highest num' do
         expect(FindSecondLargest.find_second_max(array)).to eq(40)
